96eef65e605cdb710acf38abd9f833dd6d7db376
DieRoche <200234561+DieRoche2@users.noreply.github.com>
2025-09-30 23:02:19 -0300

    configs setting

--- args.py
+++ args.py
@@ -2,17 +2,17 @@ import argparse
 import numpy as np
 from pathlib import Path
 
-parser = argparse.ArgumentParser(description="Federated Learning Simulation")
+parser = argparse.ArgumentParser(description="Federated Learning Experiments")
 
 ## FL
-parser.add_argument("--num_rounds", type=int, default=2,help="number of rounds for a federated learning training")
+parser.add_argument("--num_rounds", type=int, default=10,help="number of rounds for a federated learning training")
 parser.add_argument("--num_clients", type=int, default=10,help="number of clients")
-parser.add_argument("--alpha",type=float, default=100,help="alpha used for LDA")
+parser.add_argument("--alpha",type=float, default=0.5,help="alpha used for LDA")
 parser.add_argument("--alpha_inf",action="store_true",help="special flag to use alpha as infinity") # uniform
-parser.add_argument("--val_ratio", type=float, default=0.0,help="validationd dataset split")
+parser.add_argument("--val_ratio", type=float, default=0.2,help="validationd dataset split")
 parser.add_argument("--dataset", type = str, default='cifar10',help="which dataset to use")
-parser.add_argument("--samp_rate", type=float, default=1.0,help="client's sample rate")
-parser.add_argument("--strategy", type = str, default='feddf',help="which strategy to use")
+parser.add_argument("--samp_rate", type=float, default=0.5,help="client's sample rate")
+parser.add_argument("--strategy", type = str, default='fedlora',help="which strategy to use")
 parser.add_argument("--fedbn", action="store_true",help="fed bn strategy for local batchnorm")
 parser.add_argument("--drop_random", action="store_true",help="drop client's results with random guess")
 parser.add_argument("--bb_only", action="store_true",help="to aggregate backbone only")
@@ -22,16 +22,16 @@ parser.add_argument("--dataset_path", type = str, default='./data',help="dataset
 parser.add_argument("--from_pretrained", action="store_true",help="secretly badx2")
 
 ## Model
-parser.add_argument("--model", type = str, default='resnet8',help="model to use (resnet18, resnet20,qresnet12)")
+parser.add_argument("--model", type = str, default='resnet18',help="model to use (resnet18, resnet20,qresnet12)")
 parser.add_argument("--feature_maps", type=int, default=16,help="number of feature maps for the model")
 parser.add_argument("--batchn", action="store_true",help="to use batch norm or group norm")
 
 ## Client
 parser.add_argument("--cl_lr", type=float, default=0.01,help="client's learning rate")
-parser.add_argument("--cl_mmt", type=float, default=0.9,help="client's momentum")
+parser.add_argument("--cl_mmt", type=float, default=0.0,help="client's momentum")
 parser.add_argument("--cl_wd", type=float, default=0.0,help="client's weight decay")
-parser.add_argument("--cl_epochs", type=int, default=1,help="number of local epochs in each client")
-parser.add_argument("--cl_bs", type=int, default=16,help="client's batch size")
+parser.add_argument("--cl_epochs", type=int, default=5,help="number of local epochs in each client")
+parser.add_argument("--cl_bs", type=int, default=128,help="client's batch size")
 parser.add_argument("--only_cpu", action="store_true",help="to force the use of only cpu in the client")
 
 ## Pruning
@@ -42,17 +42,17 @@ parser.add_argument("--prate", type=float, default=0.1,help="pruning rate")
 ## Others
 parser.add_argument("--milestones", type=int, default=0, nargs='+',
     help="milestones for cl_lr, can be int (then milestones every X epochs) or list. 0 means no milestones")
-parser.add_argument("--lr_step", type=float,default=0.1,help="reduction for the learning rate")
+parser.add_argument("--lr_step", type=float,default=0.0,help="reduction for the learning rate")
 parser.add_argument("--path_results", type=str,default="results/",help="folder to save results")
 parser.add_argument("--id_exp", type=str,default="",help="id for multiple runs")
 parser.add_argument("--no_thread", action="store_true",help="to launch the fit/eval in the current thread")
 parser.add_argument("--file_name", type=str,default="",help="experience's name (optional)")
 parser.add_argument("--skip_gen_training", action="store_true",help="to skip training pt data")
 parser.add_argument("--checkpoint", action="store_true",help="secretly bad")
-parser.add_argument("--seed", type=int,default=1234,help="reduction for the learning rate")
+parser.add_argument("--seed", type=int,default=5,help="reduction for the learning rate")
 parser.add_argument("--freq_checkpoint", type=int,default=9999999,help="reduction for the learning rate")
-parser.add_argument("--nworkers", type=int,default=2,help="num workers dataloader")
-parser.add_argument("--ray_gpu", type=float, default=0.005,help="ray argument for num_gpus")
+parser.add_argument("--nworkers", type=int,default=1,help="num workers dataloader")
+parser.add_argument("--ray_gpu", type=float, default=0.3,help="ray argument for num_gpus")
 
 ##Lora
 parser.add_argument("--lora_alpha", type=int,default=16,help="alpha parameter in a lora adapter")
@@ -74,7 +74,7 @@ parser.add_argument("--quant_bits", type=int,default=8,help="number of bits for 
 ## Wandb
 parser.add_argument("--wandb", action="store_true",help="to used wandb")
 parser.add_argument("--entity", type=str,help="wandb entity name")
-parser.add_argument("--wandb_prj_name", type=str,help="wandb project name")
+parser.add_argument("--wandb_prj_name", type=str,help="compression_FL")
 
 ##FedProx
 parser.add_argument("--mu", type=float,default=0.01,help="fedprox proximal factor")
